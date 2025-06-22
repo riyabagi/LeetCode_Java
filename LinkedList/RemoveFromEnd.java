dc20e5ac7fc9eb572fe51030f2bd4e9e5c1f2cca
riya bagi <riyabagi85@gmail.com>
2025-06-22 11:16:40 +0530

    LeetCode 24: Swap Nodes in Pairs and modified RemoveFromEnd

--- LinkedList/RemoveFromEnd.java
+++ LinkedList/RemoveFromEnd.java
@@ -1,3 +1,22 @@
-public class RemoveFromEnd {
-    
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head; 
+        ListNode curr = head;
+        int count = 0;
+
+        while (curr != null) {
+            count++;
+            curr = curr.next;
+        }
+
+        curr = dummy;
+        for (int i = 0; i < count - n; i++) {
+            curr = curr.next;
+        }
+
+        curr.next = curr.next.next;
+
+        return dummy.next;
+    }
 }
---
+++ LinkedList/SwapNodes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (head != null && head.next != null) {
+            ListNode first = head;
+            ListNode second = head.next;
+
+            // Swapping
+            prev.next = second;
+            first.next = second.next;
+            second.next = first;
+
+            // Moving pointers forward
+            prev = first;
+            head = first.next;
+        }
+
+        return dummy.next;
+    }
+}
