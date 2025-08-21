fbf7a80b0b2b8d7bb93152ad8375611e7e1a1d88
riya bagi <riyabagi85@gmail.com>
2025-08-21 11:13:32 +0530

    LeetCode : Preorder Iterative Traversal of Binary Tree

--- Tree/PreorderIterativeTraversal.java
+++ Tree/PreorderIterativeTraversal.java
@@ -1,3 +1,41 @@
-public class PreorderIterativeTraversal {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            result.add(root.val);
+
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+        }
+
+        return result;
+    }
 }
